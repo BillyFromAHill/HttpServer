61ce8cf33aa8b7d0566304325754b51ef38131f3
ivan.deev <ivan.deev@gmail.com>
2016-07-17 19:55:16 +0300

    Add getopt.

--- HttpServer/HttpServer/HttpServer.cpp
+++ HttpServer/HttpServer/HttpServer.cpp
@@ -50,6 +50,38 @@ int main(int argc, char* argv[])
     }
 
 #ifdef __linux__
+
+    int aflag = 0;
+    int bflag = 0;
+    char *cvalue = NULL;
+    int index;
+    int c;
+
+    opterr = 0;
+    while ((c = getopt (argc, argv, "abc:")) != -1)
+        switch (c)
+    {
+        case 'a':
+            aflag = 1;
+            break;
+        case 'b':
+            bflag = 1;
+            break;
+        case 'c':
+            cvalue = optarg;
+            break;
+        case '?':
+            if (optopt == 'c')
+                fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+            else if (isprint(optopt))
+                fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+            else
+                fprintf(stderr,
+                "Unknown option character `\\x%x'.\n",
+                optopt);
+            return 1;
+    }
+
     pid_t parpid;
     if((parpid=fork())<0)
     {
